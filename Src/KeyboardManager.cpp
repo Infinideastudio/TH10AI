9388c79051604a3fcb3dc29a2c558cf186aa0d56
DREAMWORLDVOID <yshliu0321@icloud.com>
2018-07-12 18:55:25 +0800

    Improve Maintainability

--- Src/KeyboardManager.cpp
+++ Src/KeyboardManager.cpp
@@ -4,15 +4,16 @@
 namespace {
     bool keystate[100];
 
-    constexpr const char KEY_SHIFT = 16;
-    constexpr const char KEY_LEFT = 37;
-    constexpr const char KEY_UP = 38;
-    constexpr const char KEY_RIGHT = 39;
-    constexpr const char KEY_DOWN = 40;
-    constexpr const char KEY_X = 88;
-    constexpr const char KEY_Z = 90;
-	constexpr const char KEY_ESCAPE = 90;
-    void sendKey(int key, bool down) noexcept {
+    constexpr const char keyShift = 16;
+    constexpr const char keyLeft = 37;
+    constexpr const char keyUp = 38;
+    constexpr const char keyRight = 39;
+    constexpr const char keyDown = 40;
+    constexpr const char keyX = 88;
+    constexpr const char keyZ = 90;
+    constexpr const char keyEscape = 90;
+
+    void sendKey(const int key, const bool down) noexcept {
         if (keystate[key] != down) {
             keystate[key] = down;
             if (down) keybd_event(key, MapVirtualKey(key, 0), 0, 0);
@@ -21,23 +22,19 @@ namespace {
     }
 }
 
-int isKeyDown(int id) { return GetAsyncKeyState(id) & 0x8000 ? 1 : 0; }
+int isKeyDown(const int id) { return GetAsyncKeyState(id) & 0x8000 ? 1 : 0; }
 
-void KeyboardManager::sendKeyInfo(int dir, bool shift, bool z, bool x) {
-    sendKey(KEY_UP, static_cast<bool>(dir & 0x8));
-    sendKey(KEY_DOWN, static_cast<bool>(dir & 0x4));
-    sendKey(KEY_LEFT, static_cast<bool>(dir & 0x2));
-    sendKey(KEY_RIGHT, static_cast<bool>(dir & 0x1));
-    sendKey(KEY_SHIFT, shift);
-    sendKey(KEY_Z, z);
-    sendKey(KEY_X, x);
+void KeyboardManager::sendKeyInfo(const int dir, const bool shift, const bool z, const bool x) {
+    sendKey(keyUp, static_cast<bool>(dir & 0x8));
+    sendKey(keyDown, static_cast<bool>(dir & 0x4));
+    sendKey(keyLeft, static_cast<bool>(dir & 0x2));
+    sendKey(keyRight, static_cast<bool>(dir & 0x1));
+    sendKey(keyShift, shift);
+    sendKey(keyZ, z);
+    sendKey(keyX, x);
 }
 
 void KeyboardManager::init() { memset(keystate, 0, sizeof(keystate)); }
 
-void KeyboardManager::pressEsc() {
-	keybd_event(KEY_ESCAPE, MapVirtualKey(KEY_ESCAPE, 0), 0, 0);
-}
-void KeyboardManager::releaseEsc() {
-	keybd_event(KEY_ESCAPE, MapVirtualKey(KEY_ESCAPE, 0), KEYEVENTF_KEYUP, 0);
-}
+void KeyboardManager::pressEsc() { keybd_event(keyEscape, MapVirtualKey(keyEscape, 0), 0, 0); }
+void KeyboardManager::releaseEsc() { keybd_event(keyEscape, MapVirtualKey(keyEscape, 0), KEYEVENTF_KEYUP, 0); }
--- Src/main.cpp
+++ Src/main.cpp
@@ -15,14 +15,14 @@ public:
     void stop() noexcept { mEndTime = steady_clock::now(); }
     void restart() noexcept { start(); }
     steady_clock::duration elapsed() const noexcept { return mEndTime - mBeginTime; }
-    int elapsed_ms() const noexcept { return static_cast<int>(duration_cast<milliseconds>(elapsed()).count()); }
-    int elapsed_s() const noexcept { return static_cast<int>(duration_cast<seconds>(elapsed()).count()); }
+    int elapsedMs() const noexcept { return static_cast<int>(duration_cast<milliseconds>(elapsed()).count()); }
+    int elapsedS() const noexcept { return static_cast<int>(duration_cast<seconds>(elapsed()).count()); }
 private:
     steady_clock::time_point mBeginTime, mEndTime;
 };
 
 
-void pauseUntilPress(const char* info, char key) {
+void pauseUntilPress(const char* info, const char key) {
     std::cout << info << std::endl;
     while (true) {
         if (isKeyDown(key))return;
@@ -34,14 +34,14 @@ int main() {
     try {
         KeyboardManager::init();
         auto game = std::make_shared<GameManager>();
-        bool quit = false;
+        const auto quit = false;
         std::cout << "准备完成" << std::endl;
         pauseUntilPress("请将焦点放在风神录窗口上，开始游戏，然后按C开启AI", 'C');
         std::cout << "已开始游戏，按P键打印估价图，Q键退出" << std::endl;
         unsigned long long frameCount = 0;
         StopWatch watch;
-        int mapOutputCount = 0;
-        double cd = 0;
+        auto mapOutputCount = 0;
+        auto cd = 0.0;
         const double maxcd = 10;
         while (!quit) {
             if (cd > 0)cd -= 1.0;
@@ -60,7 +60,7 @@ int main() {
             frameCount++;
             game->update(frameCount);
             watch.stop();
-            std::this_thread::sleep_for(milliseconds(std::max(0, 16 - watch.elapsed_ms())));
+            std::this_thread::sleep_for(milliseconds(std::max(0, 16 - watch.elapsedMs())));
         }
         KeyboardManager::sendKeyInfo(0, false, false, false);
     }
